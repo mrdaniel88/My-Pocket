dd4b07d5bdd74ddae3fdf25b694534027742a7c1
Daniel Martinez <d.martinez74@gmail.com>
2023-07-18 17:06:29 +0000

    improved navbar

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,25 +8,25 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="parent">
+		<div >
+
 			
-			<Sidebar />
-			<div className="page-body">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div classNameName="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-			</div>
 			
+				<h1>Keep track of your incomes and expenses</h1>
+				<p>
+					<img src={rigoImageUrl} />
+				</p>
+				<div classNameName="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				<p>
+					This boilerplate comes with lots of documentation:{" "}
+					<a href="https://start.4geeksacademy.com/starters/react-flask">
+						Read documentation
+					</a>
+				</p>
+			
+
 		</div>
 	);
 };
