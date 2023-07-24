194d56adb992a074e833a9ee94d9afc567e0db39
Daniel Martinez <d.martinez74@gmail.com>
2023-07-24 20:05:03 +0000

    home with login

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,24 +8,36 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div >
 
-			
-			
-				<h1>Keep track of your incomes and expenses</h1>
-				<p>
-					<img src={rigoImageUrl} />
-				</p>
-				<div classNameName="alert alert-info">
-					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="text-center mt-5">
+			<h1>Keep track of your incomes and expenses</h1>
+
+			<div className="mt-5 d-flex justify-content-center">
+				<div className="card p-5 d-flex flex-column align-items-center">
+					<div className="card-body ">
+						<div className="login-form ">
+							<input type="email" className="form-control" placeholder="Email" aria-label="Email"></input>
+							<input type="password" className="form-control mt-2" placeholder="Password" aria-label="Password"></input>
+							<button className="btn mt-4 me-2" type="submit">Login</button>
+							<button className="btn mt-4" type="submit">Signup</button>
+						</div>
+					</div>
 				</div>
-				<p>
-					This boilerplate comes with lots of documentation:{" "}
-					<a href="https://start.4geeksacademy.com/starters/react-flask">
-						Read documentation
-					</a>
-				</p>
-			
+			</div>
+
+			{/* <p>
+				<img src={rigoImageUrl} />
+			</p>
+			<div classNameName="alert alert-info">
+				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			</div>
+			<p>
+				This boilerplate comes with lots of documentation:{" "}
+				<a href="https://start.4geeksacademy.com/starters/react-flask">
+					Read documentation
+				</a>
+			</p> */}
+
 
 		</div>
 	);
